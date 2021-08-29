9b675d774f8dad3d7e66b3b01a7adb5848f653bf
AntagonisticVision <GrandAntagonist@gmail.com>
2021-08-29 13:01:02 -0400

    8/29/21
    Init commit 8/29/21

---
+++ Assets/Scenes/shooter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    using UnityEngine;
+using System.Collections;
+ 
+public class ShootDemo : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public float speed = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+            instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+        }
+    }
+}
